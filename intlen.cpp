3963d8b9d64263f16322c3adee72d22b31969d94
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-04 16:36:23 +0000

    .

--- intlen.cpp
+++ intlen.cpp
@@ -8,17 +8,18 @@ int main(int argc, char * argv [])
         printf("ERROR: please provide 1 more extra argument\n");
         printf("Usage: intlen (NUMBER)\n");
         return 1;
-    } 
-    
+    }
+
     if (atoi(argv[1]) > 2000000000 || atoi(argv[1]) < 0) {
         printf("ERROR: the number can be grater then '2000000000'\n");
         printf("NOTE: your must use the library for large numbers!\n");
         return 1;
     }
-  
+
     std::size_t output = mdl::intlen(std::atoi(argv[1]));
 
     printf("%ld\n", output);
 
     return 0;
 }
+
--- src/intlen.hpp
+++ src/intlen.hpp
@@ -2,16 +2,9 @@
 # define __intlen__hpp
 # include <cstdint>
 # include <boost/cstdint.hpp>
+# include <eint_t.hpp>
 namespace mdl
 {
-    # ifdef ARC64
-        typedef boost::uint64_t uint_t;
-    # elif ARC32
-        typedef boost::uint32_t uint_t;
-    # else
-        typedef unsigned int uint_t;
-    # endif
-
     std::size_t intlen(uint_t __uint);
 }
 
